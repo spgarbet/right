2460bc57643912f2df264d07b44453eaae9f0197
graveja0 <john.graves@vanderbilt.edu>
2016-05-19 09:53:10 -0500

    Adding capability to look at initial and final attributes.

--- simulation-files/PGx-attributes.r
+++ simulation-files/PGx-attributes.r
@@ -1,15 +1,21 @@
+####################################################################################################################
+####################################################################################################################
+
 ######
 ## This sub-routine assigns patient genotyping status (Source: PREDICT Prognositic Model)
-vSensitivityPrDAPT = 0.74
-vSpecificityPrDAPT = 0.61
+inputs$vSensitivityPrDAPT = 0.74
+inputs$vSpecificityPrDAPT = 0.61
 
 #####
 ## CYP2C19 Parmeters (Source: Annals Paper)
-vCYP2C19.Poor = 0.21
-vCYP2C19.Rapid = 0.33
-vCYP2C19.Unknown = 0.07
-vCYP2C19.Probs = c(vCYP2C19.Poor,vCYP2C19.Rapid,vCYP2C19.Unknown,1-vCYP2C19.Poor-vCYP2C19.Rapid-vCYP2C19.Unknown )
+inputs$vCYP2C19.Poor = 0.21
+inputs$vCYP2C19.Rapid = 0.33
+inputs$vCYP2C19.Unknown = 0.07
+inputs$vCYP2C19.Probs = c(inputs$vCYP2C19.Poor,inputs$vCYP2C19.Rapid,inputs$vCYP2C19.Unknown,1-inputs$vCYP2C19.Poor-inputs$vCYP2C19.Rapid-inputs$vCYP2C19.Unknown )
 
+
+####################################################################################################################
+####################################################################################################################
 # The Probability of Being Genotyped (given a risk score above the threshold is 100%), unless the scenario has genotyping turned off.
 if (inputs$Scenario=="PGx-Prospective") vPrGenotyped = 1 else
   vPrGenotyped=0
@@ -18,7 +24,7 @@ assign_PGx_attributes <- function(traj,inputs=list())
 {
   traj %>%
     # First, Get the Probaiblity That the Risk Score Returns a Value Above the Threshold
-    set_attribute("aPrDAPT.Score.Eq1",  function(attrs) vSensitivityPrDAPT * (attrs[['eDAPTInitialized']]<=3650) + (1-vSpecificityPrDAPT)*(1-(attrs[["eDAPTInitialized"]]<3650))) %>%
+    set_attribute("aPrDAPT.Score.Eq1",  function(attrs) inputs$vSensitivityPrDAPT * (attrs[['aTimeDAPTInitialized']]<=3650) + (1-inputs$vSpecificityPrDAPT)*(1-(attrs[["aTimeDAPTInitialized"]]<3650))) %>%
     
     # If this is a Prospective Genotyping Arm, then the person is genotyped with some probability
     branch(
@@ -40,7 +46,7 @@ assign_CYP2C19_status <- function(traj,inputs=list())
 {
   traj %>%
     branch(
-    function() sample(1:4,1,prob=vCYP2C19.Probs),
+    function() sample(1:4,1,prob=inputs$vCYP2C19.Probs),
     merge= rep(TRUE,4),
     create_trajectory()  %>% set_attribute("aCYP2C19",1),
     create_trajectory() %>% set_attribute("aCYP2C19",2),
---
+++ simulation-files/inputs-sensitivity.r
@@ -0,0 +1,6 @@
+#######################################
+# You can modify the inputs here
+# to perform sensitivity analyses,
+# or alternatively for value of
+# information anlayses
+#######################################

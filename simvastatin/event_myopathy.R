73c3216a381996d0d2f2623447676f7180ff815e
Shawn Garbett <shawn@garbett.org>
2016-07-07 17:46:31 -0500

    Working mild myopathy

--- simvastatin/event_myopathy.R
+++ simvastatin/event_myopathy.R
@@ -1,29 +1,21 @@
 library(simmer)
 
-# FIXME FIXME FIXME
-# Past end of life
-past_end_of_life <- 365*120
 
 switch_statin <- function(inputs)
 {
   create_trajectory("Switch Statin") %>% 
     branch(
       function(attrs) attrs[["aCVDdrug"]]+1,
-      continue=rep(TRUE,4),
+      continue=rep(TRUE,3),
       create_trajectory() %>% timeout(0), # Already no treatment
       create_trajectory("Switch to Second Line") %>%
         mark("sim_switched") %>%
         release("simvastatin") %>%
         seize("alt_simvastatin") %>%
         set_attribute("aCVDdrug", 2),
-      create_trajectory("Switch to Low Dose") %>%
-        mark("switched") %>%
-        release("alt_simvastatin") %>%
-        seize("reduced_simvastatin") %>%
-        set_attribute("aCVDdrug", 3) %>%
       create_trajectory("Stopped Treatment") %>%
-        mark("stopped")  %>%
-        release("reduced_simvastatin") %>%
+        mark("stopped") %>%
+        release("alt_simvastatin") %>%
         set_attribute("aCVDdrug", 0)
   )
 }
@@ -33,73 +25,53 @@ stop_cvd_treatment <- function(inputs)
   create_trajectory("Stop CVD Treatment") %>%
     branch(
       function(attrs) attrs[["aCVDdrug"]]+1,
-      continue=rep(TRUE,4),
+      continue=rep(TRUE,3),
       create_trajectory() %>% timeout(0), # Already no treatment
       create_trajectory() %>% mark("sim_stopped")  %>% release("simvastatin"),
-      create_trajectory() %>% mark("sim_stopped")  %>% release("alt_simvastatin"),
-      create_trajectory() %>% mark("sim_stopped")  %>% release("reduced_simvastatin")
+      create_trajectory() %>% mark("sim_stopped")  %>% release("alt_simvastatin")
     ) %>%
     set_attribute("aCVDdrug", 0)
 }
 
-decrease_statin_dose <- function(inputs)
-{
-  create_trajectory("Decrease Statin Dose") %>%
-    branch(
-      function(attrs){
-        drug <- attrs[['aCVDdrug']]
-        if(drug == 0) {return(1)} else # No treatment now
-        if(drug == 3) {return(2)} else # On Low dose currently, -> Stop
-        return(3)                      # This is the intent, other TX -> Low Dose
-      },
-      continue=rep(TRUE,3),
-      create_trajectory() %>% timeout(0), # Already no treatment
-      stop_cvd_treatment(),
-      create_trajectory("Decreasing") %>%
-        mark("sim_switched")           %>%
-        stop_cvd_treatment()           %>%
-        seize("reduced_simvastatin")   %>%
-        set_attribute("aCVDdrug", 3)
-    )
-}
 
 next_step <- function(traj, inputs)
 {
   traj %>%
   branch(
-    function() sample(1:3, 1, prob=c(0.591, 0.23, 0.179)),
-    continue=rep(TRUE,3),
+    function() sample(1:2, 1, prob=c(0.77, 0.23)),
+    continue=rep(TRUE,2),
     switch_statin(inputs),
-    stop_cvd_treatment(inputs),
-    decrease_statin_dose(inputs)
+    stop_cvd_treatment(inputs)
   )
 }
 
 # Mild Myopathy events
 days_till_mild_myopathy <- function(attrs, inputs)
 {
+  sim  <- inputs$simvastatin
   drug <- attrs[["aCVDdrug"]]
   geno <- attrs[["aCVDgenotype"]]
   
+
   time_frame <- 1825 # 5 Years in days
-  risk       <- if     (drug == 0) inputs$vMildMyoBaseNoVar
-                else if(drug == 1) inputs$vMildMyoSimNoVar
-                else if(drug == 2) inputs$vMildMyoAltNoVar
-  
+  risk       <- if     (drug == 0) sim$vMildMyoBaseNoVar
+                else if(drug == 1) sim$vMildMyoSimNoVar
+                else if(drug == 2) sim$vMildMyoAltNoVar
+
   rr         <- if      (geno == 1) 1
-                else if (geno == 2 && drug == 1) inputs$vMildMyoSimMedVar
-                else if (geno == 2 && drug == 2) inputs$vMildMyoAltMedVar
-                else if (geno == 3 && drug == 1) inputs$vMildMyoSimPoorVar
-                else if (geno == 3 && drug == 2) inputs$vMildMyoAltPoorVar 
-                else if (attrs[['aCVDgenotype']] == 3) inputs$vMildMyoSimMedVar
-  
+                else if (drug == 0) 1
+                else if (geno == 2 && drug == 1) sim$vMildMyoSimMedVar
+                else if (geno == 2 && drug == 2) sim$vMildMyoAltMedVar
+                else if (geno == 3 && drug == 1) sim$vMildMyoSimPoorVar
+                else if (geno == 3 && drug == 2) sim$vMildMyoAltPoorVar 
+                else stop("Unhandled mild myopathy geno/drug combination")
+
   rate       <- -log(1-risk)*rr/time_frame
-  
   t2e <- rexp(1, rate)
-  
+
   # NOTE: Events are considered to only be in the first year. (but odds were for 5!?)
-  if(t2e > 365) {return(past_end_of_life)}
-  
+  if(t2e > 365) {return(inputs$vHorizon*365+1)}
+
   return(t2e)
 }
 
@@ -112,7 +84,7 @@ mild_myopathy <- function(traj, inputs)
 }
 
 # Moderate myopathy events
-days_till_mod_myopathy <- function(attrs)
+days_till_mod_myopathy <- function(attrs, inputs)
 {
   drug <- attrs[["aCVDdrug"]]
   gt   <- attrs[["CVDgenotype"]]
@@ -135,13 +107,13 @@ days_till_mod_myopathy <- function(attrs)
   t2e <- rexp(1, rate)
   
   # Events are considered to only be in the first year, otherwise beyond end of life
-  if(t2e > 365) {return(past_end_of_life)}
+  if(t2e > 365) {return(inputs$vHorizon*365+1)}
   
   return(t2e)
 }
 
 # Mark a moderate myopathy event
-mod_myopathy <- function(traj)
+mod_myopathy <- function(traj,inputs)
 {
   traj %>%
   mark("mod_myopathy") %>%
@@ -149,7 +121,7 @@ mod_myopathy <- function(traj)
 }
 
 # Severe myopathy events
-days_till_sev_myopathy <- function(attrs)
+days_till_sev_myopathy <- function(attrs,inputs)
 {
   drug <- attrs[["aCVDdrug"]]
   gt   <- attrs[["CVDgenotype"]]
@@ -172,13 +144,13 @@ days_till_sev_myopathy <- function(attrs)
   t2e <- rexp(1, rate)
   
   # Events are considered to only be in the first year, otherwise beyond end of life
-  if(t2e > 365) {return(past_end_of_life)}
+  if(t2e > 365) {return(inputs$vHorizon*365+1)}
   
   return(t2e)
 }
 
 # Mark a severe myopathy event
-sev_myopathy <- function(traj)
+sev_myopathy <- function(traj,inputs)
 {
   traj %>%
   mark("sev_myopathy") %>%

6399d8e940c0e48232589335c3c5328cf6913f3e
Shawn Garbett <Shawn.P.Garbett@Vanderbilt.edu>
2016-07-08 09:21:01 -0500

    Adding newer cost code to project

--- costs.R
+++ costs.R
@@ -1,27 +1,19 @@
 #####################################################
 # Costing Algorithm
 #
-library(boot)
-
 costs <- function(arrivals)
 {
-  stats <- do.call(rbind, lapply(split(arrivals, arrivals$name), FUN=function(x)
+  stats <- as.data.frame(do.call(rbind, lapply(split(arrivals, arrivals$name), FUN=function(x)
   {
     life <- x[x$resource=="life",]
     results <- c(sum(x$discounted_cost),
                 (sum(life$discounted_time) - sum(x$disutility))/365,
                 sum(life$activity_time)/365)
-    names(results) <- c("Discounted Cost", "QALY", "Life")
+    names(results) <- c("Discount.Cost", "QALY", "Life")
     results
-  }))
-
-  bo <- boot(stats, R=500, statistic=function (x,idx){
-  colSums(x[idx,])/length(idx)
-  })
+  })))
   
-  dc95   <- boot.ci(bo, type="basic", index=1)
-  qaly95 <- boot.ci(bo, type="basic", index=2)
-  life95 <- boot.ci(bo, type="basic", index=3)
+  stats$ce_ratio <- stats$Discount.Cost / stats$QALY
   
-  list(stats=stats,boot=bo,disc_cost95=dc95,qaly95=qaly95,life95=life95)
+  stats
 }

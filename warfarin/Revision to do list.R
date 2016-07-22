2e9654e3a98d9230f55bf526017a1200a1861dc8
Zilu Zhou <zilu.zhou@vanderbilt.edu>
2016-07-22 12:02:53 -0500

    to do list

---
+++ warfarin/Revision to do list.R
@@ -0,0 +1,23 @@
+to do list: 7/22
+prepare some initial outputs for review next Friday
+
+initial INR:
+  change distribution
+  change assignment
+
+t2warfarin:
+  wait for clarification in weibull distribution parameters
+
+bleeding events:
+  add minor bleeding event and adjust risks inputs (/4 to reflect INR duration shorter than 1 year)
+
+DVTPE: 
+  delete the part that randomly have people who experience non-fatal DVTPE to stop warfarin
+  need to revise risks for non-AF 
+  
+INR testing:
+  costs/disutility computation
+  
+when to release warfarin:
+  does not matter after 90 days since genotyoped vs. non-genotyped groups converge.
+  

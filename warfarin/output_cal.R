8f1de2f42fd272a063a6230f74e542b87f3de1a0
Zilu Zhou <zilu.zhou@vanderbilt.edu>
2016-10-04 09:08:35 -0500

    Updated additional warfarin codes
    generate mean time in therapeutic range and mean count of INR test.

--- warfarin/output_cal.R
+++ warfarin/output_cal.R
@@ -1,31 +1,19 @@
-warfarin_out <- function(env,inputs) 
+warfarin_out <- function(raw,inputs) 
 {
-  attributes <- arrange(get_mon_attributes(env),name,key,time) %>%  mutate(name = paste0(name,"_",replication))
-  last.attributes  <- spread(attributes %>% group_by(name,key) %>% summarize(last = last(value)),key,last)
-  dt_INR <- last.attributes %>% select(initial = aINRInitial, end = aINR, aWarfarinIndication) %>% filter(initial>0)
-  
-  ind_tab <- table(dt_INR$aWarfarinIndication)
-  
-  INR_cat <- data.frame(cat=c("I_below2","I_2to3","I_3to4","I_over4",
-                             "E_below2","E_2to3","E_3to4","E_over4"),
-                       val=c(sum(dt_INR$initial<2)/nrow(dt_INR),
-                             sum(dt_INR$initial>=2 & dt_INR$initial<=3)/nrow(dt_INR),
-                             sum(dt_INR$initial>3 & dt_INR$initial<=4)/nrow(dt_INR),
-                             sum(dt_INR$initial>4)/nrow(dt_INR),
-                             sum(dt_INR$end<2)/nrow(dt_INR),
-                             sum(dt_INR$end>=2 & dt_INR$end<=3)/nrow(dt_INR),
-                             sum(dt_INR$end>3 & dt_INR$end<=4)/nrow(dt_INR),
-                             sum(dt_INR$end>4)/nrow(dt_INR)))
-  
-  events <- events
-  out.start <- events %>% filter(resource=="Initial_OutRange") %>% select(name) #those initial out of range
-  in.end <- events %>% filter(resource=="in_range") %>% select(name) #those in range initial/later
-  out.list1 <- merge(in.end, out.start, by="name") 
-  out.end <- events %>% filter(resource=="out_of_range") %>% select(name,activity_time) #time out of range
-  out.list <- merge(out.list1,out.end, by="name") %>% arrange(activity_time)
-  sum_t2e <- summary(out.list$activity_time, digits=5)  
-  
-  out=list(indication=ind_tab,INR_cat=INR_cat,t2inrange=sum_t2e)
-  return(out)
+  if(inputs$vDrugs$vWarfarin==TRUE) {
+  t1 <- raw %>% filter(resource %in% c("in_range","out_of_range")) %>% 
+    select(name, preemptive, reactive, resource, activity_time) %>%
+    arrange(name) #in/out time among warfarin users
+  t2 <- t1 %>% spread(resource, activity_time, fill=0) #long to wide
+  r1 <- t2 %>% mutate(tot=out_of_range+in_range) %>% mutate(pctp=in_range/tot*100) %>% 
+    group_by(preemptive,reactive) %>% dplyr::summarise(mean(pctp)) %>% data.frame()
+  r2 <- t2 %>% mutate(test=out_of_range/3+in_range/7) %>%
+    group_by(preemptive,reactive) %>% dplyr::summarise(mean(test)) %>% data.frame()
+  warfarin_sup <- merge(r1,r2,by=c("preemptive","reactive"))
+  return(warfarin_sup)
+  }
+  else{print("Warfarin model's off.")}
 }
+
+
 

3c5b2fe80232ca022bbbd7e6e6c98fa54dbbc325
Zilu Zhou <zilu.zhou@vanderbilt.edu>
2017-02-13 16:29:04 -0600

    pre-draw physician behavior
    preemptive and reactive strategies used to go through different random
    draw steps, which resulted  in different random number for drug
    indication.

--- warfarin/event_in_range.R
+++ warfarin/event_in_range.R
@@ -5,12 +5,12 @@ days_till_in_range <- function(attrs, inputs)
   cat = attrs[["aInRange"]]
   PGx =  attrs[["aGenotyped_Warfarin"]]
   ava =  attrs[["aWTestAvail"]]
-  use = attrs[["aReadWarfarinTest"]]
+  read = attrs[["aControlWar2"]]
   
   if (switch==1 & cat==2) #must be on warfarin and currently out of range
   {
-    if (PGx==1 & ava==1 & use==1)             {t2e <- rexp(1, inputs$warfarin$vMedianTimetoINR_PGx)} #genotype-guided dosing: no delay (test available at hand)
-    else if (PGx==1 & ava==2 & use==1)        {t2e <- rexp(1, inputs$warfarin$vMedianTimetoINR_PGx_delay)} #genotype-guided dosing: with 3-day delay (reactive test this time) 
+    if (PGx==1 & ava==1 & read==2)             {t2e <- rexp(1, inputs$warfarin$vMedianTimetoINR_PGx)} #genotype-guided dosing: no delay (test available at hand)
+    else if (PGx==1 & ava==2)        {t2e <- rexp(1, inputs$warfarin$vMedianTimetoINR_PGx_delay)} #genotype-guided dosing: with 3-day delay (reactive test this time) 
     else                                      {t2e <- rexp(1, inputs$warfarin$vMedianTimetoINR)} #usual care (not tested at all, or physicians do not read)
   }
   else {t2e <- inputs$vHorizon*365+1} 

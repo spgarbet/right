50b077cf962a555694f4adc3b0d3fc6df74005bc
Shawn Garbett <Shawn.P.Garbett@Vanderbilt.edu>
2017-06-02 15:55:48 -0500

    Fixed simmer/dplyr select conflict

--- shiny/warfarin/output_cal.R
+++ shiny/warfarin/output_cal.R
@@ -3,7 +3,7 @@ warfarin_out <- function(raw,inputs)
   li <- unique(raw$resource)
   if(any(li=="warfarin")) {
   t1 <- raw %>% filter(resource %in% c("in_range","out_of_range")) %>% 
-    select(name, preemptive, reactive, resource, activity_time) %>%
+    dplyr::select(name, preemptive, reactive, resource, activity_time) %>%
     arrange(name) #in/out time among warfarin users
   t2 <- t1 %>% spread(resource, activity_time, fill=0) #long to wide
   r1 <- t2 %>% mutate(tot=out_of_range+in_range) %>% mutate(pctp=in_range/tot*100) %>% 
